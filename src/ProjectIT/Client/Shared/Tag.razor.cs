3a27d27e5885d380bfa2a61ab4a7b04cdc57e9b7
Joachim Alexander Kofoed <jkof@itu.dk>
2023-03-17 02:08:10 +0100

    Tag component WIP

---
+++ src/ProjectIT/Client/Shared/Tag.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ProjectIT.Client.Shared;
+
+public partial class Tag
+{
+    [Parameter]
+    public string Text { get; set; } = string.Empty;
+}
