75f7b62885962b284718a2b5ef5a4b9a024c4cab
joac1144 <jak-ak-k@hotmail.com>
2023-03-18 11:19:21 +0100

    Fix authentication, set up Bootstrap, and create tag component (#7)
    * Add Bootstrap to project
    
    * Tag component WIP
    
    * Fix Authentication
    
    * Actually fix Authentication and Authorization
    
    * Add image
    
    * Add 2 Languages and 1 Topic
    
    * Add Bootstrap properly and fix Tag component
    
    * Update README
    
    * Add reference to Microsoft.Build.Framework
    
    * Try adding condition to only run on Windows

---
+++ src/ProjectIT/Client/Shared/Tag.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ProjectIT.Client.Shared;
+
+public partial class Tag
+{
+    [Parameter]
+    public string Text { get; set; } = string.Empty;
+
+    private void OnTagClicked()
+    {
+        // Logic to remove tag from current search.
+    }
+}
