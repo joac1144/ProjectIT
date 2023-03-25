d45d69bcc5fe51fa35dbe419d447fab2a8a793f6
Joachim Alexander Kofoed <jkof@itu.dk>
2023-03-25 17:21:06 +0100

    Apply changes to new codebase

---
+++ src/ProjectIT/Client/Components/Filter/Models/FilterTag.cs
@@ -0,0 +1,12 @@
+﻿using ProjectIT.Shared.Enums;
+
+namespace ProjectIT.Client.Components.Filter;
+
+public class FilterTag
+{
+    public string Tag { get; set; } = null!;
+
+    public bool Selected { get; set; }
+
+    public TopicCategory? Category { get; set; }
+}
