336516f5f91fe32f433469e66af12109689d4c6d
Joachim Alexander Kofoed <jkof@itu.dk>
2023-03-25 17:33:20 +0100

    Use components on test page

--- src/ProjectIT/Client/Pages/TestPage.razor.cs
+++ src/ProjectIT/Client/Pages/TestPage.razor.cs
@@ -1,6 +1,18 @@
-﻿namespace ProjectIT.Client.Pages;
+﻿using ProjectIT.Client.Components.Filter;
+
+namespace ProjectIT.Client.Pages;
 
 public partial class TestPage
 {
+    public IList<FilterTag> Tags { get; set; } = new List<FilterTag>();
 
+    private void FilterPanelsInitialized(IList<FilterTag> data)
+    {
+        Tags = Tags.Concat(data).ToList();
+    }
+
+    private void OnTagClickedInFilterPanel(FilterTag filterTag)
+    {
+        Tags.Where(ft => ft.Tag == filterTag.Tag).Single().Selected = filterTag.Selected;
+    }
 }
