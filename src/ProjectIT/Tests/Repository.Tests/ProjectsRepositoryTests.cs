4cc591252123c84aa856ce7dcef54afaf1a438e4
Alaa Abdul-Al <58032582+Alaa0129@users.noreply.github.com>
2023-03-29 17:58:27 +0200

    Fix unit tests (#25)
    * Add unit tests for but one test is failing
    
    * Fix unit tests done
    
    ---------
    
    Co-authored-by: Joachim Alexander Kofoed <jak-ak-k@hotmail.com>

--- src/ProjectIT/Tests/Repository.Tests/ProjectsRepositoryTests.cs
+++ src/ProjectIT/Tests/Repository.Tests/ProjectsRepositoryTests.cs
@@ -1,20 +1,112 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using ProjectIT.Server.Database;
+using ProjectIT.Server.Repositories;
+using ProjectIT.Shared.Dtos.Projects;
+using ProjectIT.Shared.Enums;
+using ProjectIT.Shared.Models;
 
 namespace Repository.Tests;
 
 public class ProjectsRepositoryTests : IDisposable
 {
     private readonly IProjectITDbContext _context;
+    private readonly IProjectsRepository _projectsRepository;
 
-    public ProjectsRepositoryTests(IProjectITDbContext context)
+    private const string project1Title = "Title";
+    private const string project2Title = "Test";
+
+    public ProjectsRepositoryTests()
     {
+        var connection = new SqliteConnection("Filename=:memory:");
+        connection.Open();
+        var builder = new DbContextOptionsBuilder<ProjectITDbContext>();
+        builder.UseSqlite(connection);
+        var context = new ProjectITDbContext(builder.Options);
+        context.Database.EnsureCreated();
+
+        context.AddRange(
+            new Project
+            {
+                Title = "Title", 
+                Description = "Description",
+                Topics = new Topic[] { },
+                Languages = new[] { Language.English },
+                Programmes = new[] { Programme.BDS },
+                Ects = Ects.Bachelor,
+                Semester = new() { Season = Season.Spring, Year = 2024 },
+                Supervisor = new() { FullName = "test", Email = "test", Topics = new Topic[] { }, Profession = "test" },
+                Students = new Student[] { }
+            },
+            new Project
+            {
+                Title = "Test",
+                Description = "Test desc",
+                Topics = new[]
+                        {
+                            new Topic
+                            {
+                                Name = "Test Topic",
+                                Category = TopicCategory.ArtificialIntelligence
+                            },
+                            new Topic
+                            {
+                                Name = "Test Topic 2",
+                                Category = TopicCategory.SoftwareEngineering
+                            }
+                        },
+                Languages = new[]
+                        {
+                            Language.English
+                        },
+                Programmes = new[]
+                        {
+                            Programme.BSWU
+                        },
+                Ects = Ects.Bachelor,
+                Semester = new()
+                {
+                    Season = Season.Spring,
+                    Year = 2023
+                },
+                Supervisor = new()
+                {
+                    FullName = "Joachim Alexander Kofoed",
+                    Email = "jkof@itu.dk",
+                    Topics = new Topic[] { },
+                    Profession = "Professor"
+                },
+                Students = new Student[] { }
+            }
+        );
+
+        context.SaveChanges();
+
         _context = context;
+        _projectsRepository = new ProjectsRepository(context);
     }
 
     [Fact]
-    public void Test1()
+    public async void ReadAllAsync_ReturnsAllProjects()
     {
+        IEnumerable<ProjectDetailsDto> result = await _projectsRepository.ReadAllAsync();
 
+        result.Should().NotBeNull().And.Match<IEnumerable<ProjectDetailsDto>>(projects => projects.First().Title == project1Title && projects.Last().Title == project2Title);
+    }
+
+    [Fact]
+    public async void ReadByIdAsync_NonExistingId_ReturnsNull()
+    {
+        ProjectDetailsDto? result = await _projectsRepository.ReadByIdAsync(420);
+        result.Should().BeNull();
+    }
+
+    [Fact]
+    public async void ReadByIdAsync_ExistingId_ReturnsProjectDetails()
+    {
+        ProjectDetailsDto? result = await _projectsRepository.ReadByIdAsync(2);
+
+        result.Should().NotBeNull().And.Match<ProjectDetailsDto>(p => p.Title == project2Title);
     }
 
     public void Dispose()
