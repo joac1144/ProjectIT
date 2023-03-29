f403cf388f84aaa6f8281ca730ce121549efbc5b
Mohammadhasham1999 <mh-baig@outlook.dk>
2023-03-29 15:43:18 +0200

    Fixed FE for view project details but BE left

--- src/ProjectIT/Client/Pages/ProjectDetails.razor.cs
+++ src/ProjectIT/Client/Pages/ProjectDetails.razor.cs
@@ -1,16 +1,29 @@
 using Microsoft.AspNetCore.Components;
 using ProjectIT.Client.Components.Filter;
+using ProjectIT.Shared.Dtos.Projects;
+using System.Net.Http.Json;
 
 namespace ProjectIT.Client.Pages;
 
-public partial class ProjectDetails 
+public partial class ProjectDetails
 {
 
+    [Parameter]
+    public int id { get; set; }
+
+    public ProjectDetailsDto project { get; set; }
+    HttpClient httpClient = new HttpClient();
     public IList<FilterTag> Tags { get; set; } = new List<FilterTag>();
 
     private void ApplyProject(NavigationManager navigationManager)
     {
         navigationManager.NavigateTo("/");
     }
-    
+
+    // protected override async Task OnInitializedAsync()
+    // {
+    //     project = await httpClient.GetFromJsonAsync<ProjectDetailsDto>("https://localhost:7094/projects/1");
+
+    // }
+
 }
