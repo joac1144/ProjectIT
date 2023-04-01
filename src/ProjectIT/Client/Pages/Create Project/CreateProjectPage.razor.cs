c8f1528e3041763f4f009a7b67fe222935b6ec8d
Alaa Abdul-Al <alia@itu.dk>
2023-04-01 02:39:59 +0200

    Done with create project page. No functionality yet.

---
+++ src/ProjectIT/Client/Pages/Create Project/CreateProjectPage.razor.cs
@@ -0,0 +1,74 @@
+using System.Net.Http.Json;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+using ProjectIT.Client.Components.Filter;
+using ProjectIT.Shared.Dtos.Projects;
+using ProjectIT.Shared.Enums;
+using ProjectIT.Shared.Models;
+
+namespace ProjectIT.Client.Pages.Create_Project;
+
+public partial class CreateProjectPage
+{
+    public IList<FilterTag> Tags { get; set; } = new List<FilterTag>();
+    
+    public IList<Project> Projects { get; set; } = new List<Project>();
+
+    public Project Project { get; set; } = new Project();
+
+    private ClaimsPrincipal authUser;
+
+    private string? descriptionHtml;
+
+    private string? tagName;
+
+    protected override async Task OnInitializedAsync()
+    {
+        authUser = (await AuthenticationStateProvider.GetAuthenticationStateAsync()).User; 
+    }
+
+    private void FilterPanelsInitialized(IList<FilterTag> data)
+    {
+        Tags = Tags.Concat(data).ToList();
+    }
+
+    private void OnTagClickedInFilterPanel(FilterTag filterTag)
+    {
+        Tags.Where(ft => ft.Tag == filterTag.Tag).Single().Selected = filterTag.Selected;
+    }
+
+    public async Task SubmitProjectAsync()
+    {
+        var project = new ProjectCreateDto()
+        {
+            Title = Project.Title,
+            Description = descriptionHtml!,
+            Topics = Project.Topics,
+            Languages = Project.Languages,
+            Programmes = Project.Programmes,
+            Ects = Project.Ects,
+            Semester = Project.Semester,
+            Supervisor = Project.Supervisor,
+            CoSupervisor = Project.CoSupervisor
+        };
+
+        var response = await httpClient.PostAsJsonAsync("projects", project);
+
+        if (response.IsSuccessStatusCode)
+        {
+            await JSRuntime.InvokeAsync<string>("alert", "Project created successfully!");
+            navManager.NavigateTo("MainPage");
+        }
+        else
+        {
+            await JSRuntime.InvokeAsync<string>("alert", "Something went wrong, check your input and try again!");
+        }
+    }
+
+    public async Task CancelProjectAsync()
+    {
+        navManager.NavigateTo("MainPage");
+    }
+
+}
