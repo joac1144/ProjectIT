dfbd03051e35b8091b244591829efa6a7dc5794d
Alaa Abdul-Al <58032582+Alaa0129@users.noreply.github.com>
2023-04-01 02:43:38 +0200

    Done with create project page. No functionality yet. (#33)

--- src/ProjectIT/Client/Program.cs
+++ src/ProjectIT/Client/Program.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ProjectIT.Client;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped<DialogService>();
 
 builder.Services.AddHttpClient("ProjectIT.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
