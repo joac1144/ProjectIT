565f121f03158281d44626a8e5757561276ebea4
Joachim Alexander Kofoed <jak-ak-k@hotmail.com>
2023-04-07 19:37:02 +0200

    Add unit tests for Topics

---
+++ src/ProjectIT/Tests/Controller.Tests/TopicsControllerTests.cs
@@ -0,0 +1,24 @@
+using Moq;
+using ProjectIT.Server.Controllers;
+using ProjectIT.Server.Repositories.Interfaces;
+using ProjectIT.Shared.Models;
+
+namespace Controller.Tests;
+
+public class TopicsControllerTests
+{
+    [Fact]
+    public async void GetAll_ReturnsProjects()
+    {
+        // Arrange.
+        var repository = new Mock<ITopicsRepository>();
+        repository.Setup(pr => pr.ReadAllAsync()).ReturnsAsync(Array.Empty<Topic>());
+        var controller = new TopicsController(repository.Object);
+
+        // Act.
+        var result = await controller.GetAll();
+
+        // Assert.
+        result.Should().BeEquivalentTo(Array.Empty<Topic>());
+    }
+}
