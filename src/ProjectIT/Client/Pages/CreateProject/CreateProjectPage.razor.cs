da60b553615ef2dd3a337c2d81e65f26f01b6469
Joachim Alexander Kofoed <jak-ak-k@hotmail.com>
2023-04-07 05:05:40 +0200

    Fix ECTS and Topics panels

--- src/ProjectIT/Client/Pages/CreateProject/CreateProjectPage.razor.cs
+++ src/ProjectIT/Client/Pages/CreateProject/CreateProjectPage.razor.cs
@@ -6,65 +6,82 @@ using ProjectIT.Client.Components.Filter;
 using ProjectIT.Shared.Dtos.Projects;
 using ProjectIT.Shared.Enums;
 using ProjectIT.Shared.Models;
+using Radzen.Blazor;
 
 namespace ProjectIT.Client.Pages.CreateProject;
 
 public partial class CreateProjectPage
 {
-    private class TopicGroupData : TopicCategory
+    private class EctsWrapper
     {
-        public bool IsGroup => Name != null;
+        public Ects Ects { get; set; }
+
+        public string StringValue => Ects.ToString();
     }
 
-    public IList<Project> Projects { get; set; } = new List<Project>();
+    private IEnumerable<Topic> topics;
+    private readonly IEnumerable<EctsWrapper> ectsWrappers = Enum.GetValues<Ects>().Select(ects => new EctsWrapper { Ects = ects });
 
-    public Project Project { get; set; } = new Project();
+    private readonly Project project = new();
+    private string? descriptionHtml;
+    private readonly List<Topic> projectTopics = new();
+
+    private RadzenDropDown<Topic>? topicSelector;
 
     private ClaimsPrincipal authUser;
-
-    private string? descriptionHtml;
-
-    private IEnumerable<TopicGroupData>? topics;
-    private Topic? currentTopic;
 
     protected override async Task OnInitializedAsync()
     {
         authUser = (await AuthenticationStateProvider.GetAuthenticationStateAsync()).User;
 
-        topics = (await httpClient.GetFromJsonAsync<IEnumerable<Topic>>("topics"))!
-                    .Select(t => new TopicGroupData { Id = t.Id, Name = t.Name, Category = t.Category });
+        topics = (await httpClient.Client.GetFromJsonAsync<IEnumerable<Topic>>("topics"))!;
+        if (topics == null)
+            throw new Exception("Could not load topics");
 
-        topics = topics?.GroupBy(t => t.Category)
-            .SelectMany(i => new TopicGroupData[] { new TopicGroupData { Category = i.Key } }
-                .Concat(i.Select(o =>
-                    new TopicGroupData
-                    {
-                        Name = o.Name
-                    })));
-
+        SortTopics();
     }
 
     private void OnTagClickedInFilterPanel(FilterTag filterTag)
     {
-        
+        // Add tag to relevant list
     }
+
+    private void OnTopicSelectedInList(object value)
+    {
+        if (value != null)
+        {
+            string val = (string)value;
+            projectTopics.Add(topics.Single(t => t.Name == val));
+            topics = topics.Where(t => t.Name != val);
+            topicSelector?.Reset();
+        }
+    }
+
+    private void OnSelectedTopicClicked(Topic topic)
+    {
+        projectTopics.Remove(topic);
+        topics = topics.Append(topic);
+        SortTopics();
+    }
+
+    private void SortTopics() => topics = topics.OrderBy(t => nameof(t.Category)).ThenBy(t => t.Name);
 
     private async Task SubmitProjectAsync()
     {
-        var project = new ProjectCreateDto()
+        var newProject = new ProjectCreateDto()
         {
-            Title = Project.Title,
+            Title = project.Title,
             Description = descriptionHtml!,
-            Topics = Project.Topics,
-            Languages = Project.Languages,
-            Programmes = Project.Programmes,
-            Ects = Project.Ects,
-            Semester = Project.Semester,
-            Supervisor = Project.Supervisor,
-            CoSupervisor = Project.CoSupervisor
+            Topics = projectTopics,
+            Languages = project.Languages,
+            Programmes = project.Programmes,
+            Ects = project.Ects,
+            Semester = project.Semester,
+            Supervisor = project.Supervisor,
+            CoSupervisor = project.CoSupervisor
         };
 
-        var response = await httpClient.PostAsJsonAsync("projects", project);
+        var response = await httpClient.Client.PostAsJsonAsync("projects", newProject);
 
         if (response.IsSuccessStatusCode)
         {
