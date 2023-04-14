b8c5932ba0dd51ca940753817cba9d52393ae53b
Alaa Abdul-Al <alia@itu.dk>
2023-04-14 02:52:22 +0200

    Fixed navmenu

--- src/ProjectIT/Client/Shared/NavMenu.razor.cs
+++ src/ProjectIT/Client/Shared/NavMenu.razor.cs
@@ -6,7 +6,13 @@ public partial class NavMenu
     private string createProject = "Create Project";
     private string createRequest = "Create Request";
     private string supervisors = "Supervisors";
-    private string activeTab = "Home";
+    private string activeTab = null!;
+
+    protected override void OnInitialized()
+    {
+        Navigation.NavigateTo("/");
+        activeTab = home;
+    }
 
     private void SetActiveTab(string val)
     {
