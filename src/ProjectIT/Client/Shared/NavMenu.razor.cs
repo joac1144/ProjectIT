f13f473d62931f24c46d02c328a4d2c0b7765d14
joac1144 <jak-ak-k@hotmail.com>
2023-04-18 22:52:42 +0200

    Authorization (#51)
    * WIP
    
    * Works but styling in NavMenu is broken
    
    * fix something with the navmenu
    
    * page roles
    
    * Delete UserClaims.razor
    
    * fixed navbar
    
    ---------
    
    Co-authored-by: Alaa Abdul-Al <58032582+Alaa0129@users.noreply.github.com>
    Co-authored-by: Alaa Abdul-Al <alia@itu.dk>

--- src/ProjectIT/Client/Shared/NavMenu.razor.cs
+++ src/ProjectIT/Client/Shared/NavMenu.razor.cs
@@ -1,21 +1,10 @@
+using ProjectIT.Client.Constants;
+
 namespace ProjectIT.Client.Shared;
 
 public partial class NavMenu
 {
-    private string home = "Home";
-    private string createProject = "Create Project";
-    private string createRequest = "Create Request";
-    private string supervisors = "Supervisors";
-    private string activeTab = null!;
+    private string activeTab = PageUrls.Projects;
 
-    protected override void OnInitialized()
-    {
-        Navigation.NavigateTo("/");
-        activeTab = home;
-    }
-
-    private void SetActiveTab(string val)
-    {
-        activeTab = val;
-    }
+    private void SetActiveTab(string val) => activeTab = val;
 }
