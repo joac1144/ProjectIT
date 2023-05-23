9aee77431bfaeaf028bbdf7273cb6702208b3f7b
Alaa Abdul-Al <alia@itu.dk>
2023-05-23 16:33:31 +0200

    Fix data

---
+++ src/ProjectIT/Server/Migrations/20230523142014_ProjectIT 8356609d-6b9e-494b-9a5a-ca09c539bc5b.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectIT.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProjectIT8356609d6b9e494b9a5aca09c539bc5b : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
