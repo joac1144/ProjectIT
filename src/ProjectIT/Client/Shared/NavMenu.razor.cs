4550d5ced1e6948973133dddc2c2e810abc1a643
Emdadollah <Emdadollah.habibollah@getinge.com>
2023-07-01 14:54:00 +0200

    fixed navbar not applaying active tab for the current page

--- src/ProjectIT/Client/Shared/NavMenu.razor.cs
+++ src/ProjectIT/Client/Shared/NavMenu.razor.cs
@@ -1,10 +1,34 @@
+using Microsoft.AspNetCore.Components;
 using ProjectIT.Client.Constants;
+using Microsoft.AspNetCore.Components.Routing;
 
 namespace ProjectIT.Client.Shared;
 
 public partial class NavMenu
 {
-    private string activeTab = PageUrls.Projects;
+    private string activeTab;
+
+
+    protected override void OnInitialized()
+    {
+        activeTab = GetActiveTabFromUrl(Navigation.Uri);
+        base.OnInitialized();
+    }
 
     private void SetActiveTab(string val) => activeTab = val;
+
+    private string GetActiveTabFromUrl(string url)
+    {
+        if (url.Contains(PageUrls.Projects))
+            return PageUrls.Projects;
+        else if (url.Contains(PageUrls.Supervisors))
+            return PageUrls.Supervisors;
+        else if (url.Contains(PageUrls.CreateRequest))
+            return PageUrls.CreateRequest;
+        else if (url.Contains(PageUrls.CreateProject))
+            return PageUrls.CreateProject;
+
+        return PageUrls.Projects;
+    }
+
 }
