208a76d86d379a4ac58bba2f2af2bbff16038e15
Mohammadhasham1999 <mh-baig@outlook.dk>
2023-08-07 03:26:01 +0200

    FE-my-profile for supervisor done

--- src/ProjectIT/Client/Components/Login/LoginDisplay.razor.cs
+++ src/ProjectIT/Client/Components/Login/LoginDisplay.razor.cs
@@ -1,9 +1,37 @@
-﻿using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+﻿using System.Net.Http.Json;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using ProjectIT.Client.Http;
+using ProjectIT.Shared;
+using ProjectIT.Shared.Dtos.Users;
+using ProjectIT.Client.Constants;
 
 namespace ProjectIT.Client.Components.Login;
 
 public partial class LoginDisplay
 {
+
+    private ClaimsPrincipal? authUser;
+    private string? userEmail;
+
+    private SupervisorDetailsDto? supervisor = new();
+
+    private async void DisplaySupervisorProfile()
+    {
+        authUser = (await authenticationStateProvider.GetAuthenticationStateAsync()).User;
+
+        userEmail = authUser?.FindFirst("preferred_username")?.Value!;
+
+        supervisor = await anonymousClient.Client.GetFromJsonAsync<SupervisorDetailsDto>($"{ApiEndpoints.Supervisors}/{userEmail}");
+
+        Console.WriteLine($"User Id: {supervisor!.Id}");
+
+        navManager.NavigateTo($"{PageUrls.MyProfile}/{supervisor!.Id}");
+
+    }
+
     public void BeginLogOut()
     {
         Navigation.NavigateToLogout("authentication/logout");
@@ -12,5 +40,8 @@ public partial class LoginDisplay
     public void BeginLogIn()
     {
         Navigation.NavigateToLogin("authentication/login");
+
     }
+
+
 }
