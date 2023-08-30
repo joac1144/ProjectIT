30a4c65ab92956f23620e98522d805672d6143d2
Joachim Kofoed <jak-ak-k@hotmail.com>
2023-08-30 05:13:22 +0200

    Created enum to be used.

---
+++ src/ProjectIT/Shared/Enums/StudentGroupStatus.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjectIT.Shared.Enums;
+
+public enum StudentGroupStatus
+{
+    /// <summary>
+    /// Status indicating that students have sent an application (projects) or request (requests) and are awaiting approval from supervisor.
+    /// </summary>
+    StudentsSentApplicationOrRequest,
+
+    /// <summary>
+    /// Status indicating that a supervisor has approved an application (projects) or request (requests) and is awaiting final confirmation from students.
+    /// </summary>
+    SupervisorApproved
+}
--- src/ProjectIT/Shared/Models/StudentGroup.cs
+++ src/ProjectIT/Shared/Models/StudentGroup.cs
@@ -1,8 +1,12 @@
-﻿namespace ProjectIT.Shared.Models;
+﻿using ProjectIT.Shared.Enums;
+
+namespace ProjectIT.Shared.Models;
 
 public class StudentGroup
 {
     public int Id { get; set; }
 
     public IEnumerable<Student> Students { get; set; } = null!;
+
+    public StudentGroupStatus Status { get; set; }
 }
